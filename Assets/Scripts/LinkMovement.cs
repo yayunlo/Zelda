01a3e4f1378132fdce6009fb8eda3cbe8c93f510
Ya-Yun Lo <yayunlo@umich.edu>
2015-09-19 15:32:18 -0400

    done Link movement

--- Assets/Scripts/LinkMovement.cs
+++ Assets/Scripts/LinkMovement.cs
@@ -8,11 +8,12 @@ public class LinkMovement : MonoBehaviour {
 	public GameObject swordInstance;
 	public int swordCooldown = 30;
 	public char currentDir = 'n';
-
+	public float positionCorrectParam = 0.025f;
+	Vector2 positionError = Vector2.zero;
+	
 
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
@@ -24,6 +25,8 @@ public class LinkMovement : MonoBehaviour {
 		if (horizontal_input != 0.0f) {
 			vertical_input = 0.0f;
 		}
+		horizontal_input = (positionError.y != 0) ? 0.0f : horizontal_input;
+		vertical_input = (positionError.x != 0) ? 0.0f : vertical_input;
 
 		if(horizontal_input > 0)	
 			currentDir = 'e';
@@ -34,21 +37,68 @@ public class LinkMovement : MonoBehaviour {
 		else if(vertical_input < 0)
 			currentDir = 's';
 
-
 		Vector3 newVelocity = new Vector3 (horizontal_input, vertical_input, 0);
 		GetComponent<Rigidbody> ().velocity = newVelocity * velocityFactor;
+
+		if (horizontal_input != 0.0f) {
+			positionError.x = (horizontal_input > 0.0f)? 1.0f : -1.0f;
+		} 
+		else if (vertical_input != 0.0f) {
+			positionError.y = (vertical_input > 0.0f)? 1.0f : -1.0f;
+		}
 
 		// Animate
 		GetComponent<Animator> ().SetFloat ("vertical_vel", newVelocity.y);
 		GetComponent<Animator> ().SetFloat ("horizontal_vel", newVelocity.x);
 
 		// If we're not moving, don't animate.
-		if (newVelocity.magnitude == 0)
+		//if (newVelocity.magnitude == 0)
+		if (positionError.x == 0.0f && positionError.y == 0.0f)
 			GetComponent<Animator> ().speed = 0.00000001f;
 		else
 			GetComponent<Animator> ().speed = 1.0f;
 
 		Combat ();
+
+		// Correct postition to correct track
+		if (positionError.x != 0.0f && horizontal_input == 0.0f) {
+			
+			Vector3 newPos = transform.position;
+
+			float idealX = Mathf.Ceil (transform.position.x / 0.5f);
+			if ( positionError.x < 0.0f) {
+				idealX = idealX - 1.0f;
+			}
+			
+			if( Mathf.Abs( idealX * 0.5f - transform.position.x ) > positionCorrectParam) {
+				newPos.x = transform.position.x + (positionError.x * positionCorrectParam);
+			}
+			else {
+				newPos.x = idealX * 0.5f;
+				positionError.x = 0.0f;
+			}
+			transform.position = newPos;
+		} 
+		if (positionError.y != 0.0f && vertical_input == 0.0f) {
+
+			Vector3 newPos = transform.position;
+
+			float idealY = Mathf.Ceil (transform.position.y / 0.5f);
+			if ( positionError.y < 0.0f) {
+				idealY = idealY - 1.0f;
+			}
+
+			if( Mathf.Abs( idealY * 0.5f - transform.position.y ) > positionCorrectParam) {
+				newPos.y = transform.position.y + (positionError.y * positionCorrectParam);
+
+			}
+			else {
+				newPos.y = idealY * 0.5f;
+				positionError.y = 0.0f;
+			}
+			transform.position = newPos;
+		} 
+
 	}
 
 	void Combat (){
@@ -89,7 +139,21 @@ public class LinkMovement : MonoBehaviour {
 	void OnCollisionEnter (Collision coll) {
 
 		if (coll.gameObject.tag == "Rupee") {
-			Destroy(coll.gameObject);
+			Destroy (coll.gameObject);
+		} 
+		else {
+			if(positionError.x != 0) {
+				Vector3 newPos = transform.position;
+				newPos.x = Mathf.Round(newPos.x / 0.5f) * 0.5f;
+				transform.position = newPos;
+				positionError.x = 0.0f;
+			}
+			if(positionError.y != 0) {
+				Vector3 newPos = transform.position;
+				newPos.y = Mathf.Round(newPos.y / 0.5f) * 0.5f;
+				transform.position = newPos;
+				positionError.y = 0.0f;
+			}
 		}
 	}
 }

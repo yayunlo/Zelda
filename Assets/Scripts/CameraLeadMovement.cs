6cc999d4fde1060893c70f901d53ed0ddd9e2c36
Ya-Yun Lo <yayunlo@umich.edu>
2015-09-20 18:50:57 -0400

    room transition through door and locked door

--- Assets/Scripts/CameraLeadMovement.cs
+++ Assets/Scripts/CameraLeadMovement.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class CameraLeadMovement : MonoBehaviour {
 
-	public GameObject Link;
-	Vector2 currRoom = Vector2.zero;
 	public float speed = 3f;
+	public GameObject Link;
+
+	Vector2 currRoom = Vector2.zero;
+	Vector3 newPos = Vector3.zero;
+	public static bool cameraMoving = true;
 
 	// Use this for initialization
 	void Start () {
 		Link = GameObject.Find("Link");
 		currRoom.x = Mathf.Floor (Link.transform.position.x / 16.0f) * 16.0f;
-		currRoom.y = Mathf.Floor (Link.transform.position.x / 11.0f) * 11.0f;
+		currRoom.y = Mathf.Floor (Link.transform.position.y / 11.0f) * 11.0f;
+		cameraMoving = false;
 	}
 	
 	// Update is called once per frame
@@ -20,8 +24,31 @@ public class CameraLeadMovement : MonoBehaviour {
 	
 		if( currRoom != LinkRoom ) {
 			float step = speed * Time.deltaTime;
-			Vector3 newPos = new Vector3(LinkRoom.x + 8f, LinkRoom.y + 5.0f, 0.0f);
+			newPos = new Vector3(LinkRoom.x + 8f, LinkRoom.y + 4.0f, 0.0f);
 			transform.position = Vector3.MoveTowards(transform.position, newPos, step);
+			cameraMoving = true;
+		}
+
+		if (cameraMoving && transform.position == newPos) {
+			currRoom = LinkRoom;
+			Vector3 linkNewPos = Link.transform.position;
+
+			if(LinkMovement.currentDir == 'n') {
+				linkNewPos.y += 1f;
+			} 
+			else if (LinkMovement.currentDir == 's' ) {
+				linkNewPos.y -= 1f;
+			}
+			else if(LinkMovement.currentDir == 'e') {
+				linkNewPos.x += 1f;
+			} 
+			else if (LinkMovement.currentDir == 'w' ) {
+				linkNewPos.x -= 1f;
+			}
+			LinkMovement.newRoomStartPos = linkNewPos;
+
+			cameraMoving = false;
+			newPos = Vector3.zero;
 		}
 
 	}

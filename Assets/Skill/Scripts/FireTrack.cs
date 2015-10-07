9582ee3906443786f4850e8d88f4affec2ba4903
Haizhou Zhao <zhaohaizhou940527@gmail.com>
2015-10-07 02:54:23 -0400

    Update for Turn-in on Wednesday

---
+++ Assets/Skill/Scripts/FireTrack.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireTrack : MonoBehaviour {
+
+	private const float lifeTime = 10.0f;
+	private float age;
+
+	// Use this for initialization
+	void Start () {
+		age = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		age += Time.deltaTime;
+		if (age >= lifeTime)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.tag == "Enemy")
+		{
+			Destroy(collider.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Skill/Scripts/FireTrackSkill.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireTrackSkill : MonoBehaviour
+{
+	public GameObject firePrefab;
+	private GameObject fireInstance;
+	private bool isSpawning;
+	private int fixedUpdateCounter;
+	private const int fireCounter = 5;
+	private const int fixedUpdatePerSecond = 20;
+
+	// Use this for initialization
+	void Start()
+	{
+		isSpawning = false;
+		fixedUpdateCounter = 0;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{
+			isSpawning = true;
+		}
+
+		if (isSpawning)
+		{
+			Vector3 updatedPosition = transform.position;
+			switch (LinkMovement.currentDir)
+			{
+				case 'n':
+					updatedPosition.y += 5 * Time.deltaTime;
+					break;
+				case 's':
+					updatedPosition.y -= 5 * Time.deltaTime;
+					break;
+				case 'e':
+					updatedPosition.x += 5 * Time.deltaTime;
+					break;
+				case 'w':
+					updatedPosition.x -= 5 * Time.deltaTime;
+					break;
+
+			}
+			transform.position = updatedPosition;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (isSpawning)
+		{
+			++fixedUpdateCounter;
+			if (fixedUpdateCounter % fixedUpdatePerSecond == 0)
+			{
+				fireInstance = Instantiate(firePrefab, transform.position, Quaternion.identity) as GameObject;
+			}
+			if (fixedUpdateCounter >= fireCounter * fixedUpdatePerSecond)
+			{
+				isSpawning = false;
+				fixedUpdateCounter = 0;
+			}
+		}
+	}
+}
